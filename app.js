3b3e4bcab3056eb2e0b3e723649d8a3fd500a3ef
unknown <gandharvesolanki@gmail.com>
2022-10-31 00:24:50 +0530

    Added seller page

--- app.js
+++ app.js
@@ -37,8 +37,132 @@ const itemSchema = new mongoose.Schema ({
     Fourth_img:String,
     Fifth_img:String
  });
- const productinputs = new mongoose.model ("productinputs", itemSchema);
- module.exports = mongoose.model('ecomDB', itemSchema,  "productinputs")
+
+ const sellerSchema = new mongoose.Schema ({
+    Seller_Name:String,
+    Logo_link:String,
+    Seller_deck:String,
+    Phone:Number,
+    Email:String,
+    Address:String,
+    password:String,
+    cnfpassword:String
+ });
+
+ const formdata = new mongoose.Schema({
+    Seller_Name: { type: String, required: true },
+    Logo_link: { type: String, required: true },
+    Seller_deck: { type: String, required: true },
+    Phone: { type: Number, required: "phone number is required", unique: true },
+    Email: { type: String, required: "email is required", unique: true },
+    Address: { type: String, required: "please enter address" },
+    password: { type: String, required: "create password" },
+    cnfpassword: { type: String, required: "confirm password" }
+});
+
+const forminputs = new mongoose.model("Sellers", formdata);
+app.get("/", function (req, res) {
+    res.sendFile(__dirname+ "/Seller_login/seller_register.html");
+});
+
+app.get("/login", function (req, res) {
+    res.sendFile(__dirname + "/Seller_login/login.html");
+});
+
+app.post("/", async function (req, res) {
+    const seconddata = new forminputs({
+        Seller_Name: req.body.full_name,
+        Logo_link: req.body.seller_image,
+        Seller_deck: req.body.Seller_description,
+        Phone: req.body.mobile_no,
+        Email: req.body.email,
+        Address: req.body.address,
+        password: req.body.cust_password,
+        cnfpassword: req.body.cust_password_repeat
+    });
+    let pwd1 = req.body.cust_password;
+    let pwd2 = req.body.cust_password_repeat;
+    //checking passwords
+    if (pwd1 === pwd2) {
+        await seconddata.save(function (err) {
+            if (err) {
+                var errmes = "Oh no! error has occoured : Email or phone number already registered, go back and try again with different credentials"
+                res.render("signin_failed", { errmessage: errmes })
+                console.log(err);
+            } else {
+                res.render("signin");
+                console.log("user data saved successfully");
+            }
+        });
+
+    }
+    else {
+        var errmes = "Passwords did not match";
+        res.render("signin_failed", { errmessage: errmes })
+        console.log("passwords do not match");
+    }
+})
+
+/* sign in system*/
+app.post("/login", async function (req, res) {
+    try {
+        const email = req.body.email;
+        const password = req.body.cust_password;
+        const usermail = await forminputs.findOne({ Email: email });
+        var id = usermail._id.toString().replace(/ObjectId\("(.*)"\)/, "$1");
+        if (usermail.password === password) {
+            res.render("signin_success", { userid: id })
+            /*res.send("<h2>you are signed in, your unique user ID is : " + id+"</h2>");*/
+        } else {
+            var errmes = "Sign-in failed. (invalid credentials!). You can go back and try again."
+            res.render("signin_failed", { errmessage: errmes })
+            /*res.send("Oops! invalid credentials,go back and try again");*/
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.send("looks like you are not registered yet");
+    }
+});
+
+/*data fetching system*/
+
+const userschema = {
+    Seller_Name: String,
+    Phone: Number,
+    Email: String,
+    Address: String,
+    password: String,
+    cnfpassword: String
+}
+
+const receiver = mongoose.model("Datainputs", userschema);
+
+app.get("/userdata/:id", function (req, res) {
+    console.log(req.params.id)
+    receiver.findById(req.params.id, function (err, user) {
+        if (err) {
+            res.render("signin_failed");
+            /*console.log(err);*/
+        } else {
+            /*console.log(user);*/
+            res.render("user-data", { users: user });
+        }
+    });
+});
+
+//  const productinputs = new mongoose.model ("productinputs", itemSchema);
+
+//  module.exports = mongoose.model('ecomDB', itemSchema,  "productinputs")
+
+const productinputs = mongoose.model("productinputs", itemSchema);
+const sellers = mongoose.model('sellers', sellerSchema);
+  
+// Exporting our model objects
+module.exports = {
+   productinputs,sellers
+}
+
 app.get('/post/:anything', (req, res) => {
     const reqTitle= req.params.anything;
    productinputs.find({ _id:reqTitle}, function (err, docs) {
@@ -53,32 +177,47 @@ app.get('/post/:anything', (req, res) => {
     });
 });
 
-// seller page data
-app.get('/seller', (req, res) => {
-    const reqTitle= req.params.anything;
-   productinputs.find({ _id:reqTitle}, function (err, docs) {
+
+
+
+app.post("/signthis",function(req,res){
+    const title=String(req.body.uid);
+    sellers.find({ _id:title}, function (err, docs) {
         if (err){
             console.log(err);
         }
         else{
-            console.log("First function call : ", docs);
+           // console.log("First function call : ", docs);
         }
       
-        res.render("product",{Title:docs});
+      docs.forEach(function(ele){
+       sellerName=ele.Seller_Name;
+      });
+      productinputs.find({ Seller_Name:sellerName}, function (err, sellerinfoarray) {
+        if (err){
+            console.log(err);
+        }
+        else{
+            console.log("First function call : ", sellerinfoarray);
+        }
+       res.render("sellerPage",{sellerarray:sellerinfoarray});
+      
     });
-});
-
-
-app.post("/",function(req,res){
-    
+    });
 })
 
-app.get("/",function(req,res){
-   res.sendFile(path.join(__dirname+"/Pages/homePage.html"));
-});
+
+// app.get("/",function(req,res){
+//    res.sendFile(path.join(__dirname+"/Pages/homePage.html"));
+// });
 
 app.get("/category",function(req,res){
     res.sendFile(path.join(__dirname+"/Pages/category.html"));
+ });
+
+app.get("/seller",function(req,res){
+    // res.sendFile(path.join(__dirname+""));
+    res.render("sellerPage");
  });
  
 
